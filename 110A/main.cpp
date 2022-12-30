82c084ff0296f0436b010135f37d9946ce536761
OfficerGreg <Jakub.Gancarz@protonmail.com>
2022-12-30 18:03:59 +0100

    Update

--- 110A/main.cpp
+++ 110A/main.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 
 int main() {
-    int n;
 
+    long long n;
     cin >> n;
+
+    int l = 0;
+
+    while (n != 0) {
+        if (n % 10 == 4 || n % 10 == 7) {
+            l++;
+        }
+
+        n = n / 10;
+
+    }
+    if (l == 4 || l == 7)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
 
     return 0;
 }
