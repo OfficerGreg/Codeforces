a412a3431aacc6e5def5aa06a182a5f093226ea6
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-01-03 11:36:39 +0100

    Update

---
+++ 800/734A/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n, anton = 0, danik = 0;
+
+    cin >> n;
+
+    string s;
+
+    cin >> s;
+
+
+    for(int i = 0; i < n; i++){
+
+        if(s[i] == 'A'){
+            anton++;
+        }
+        if(s[i] == 'D'){
+            danik++;
+        }
+    }
+    if(anton > danik){
+        cout << "Anton\n";
+    }else if(anton < danik){
+        cout << "Danik\n";
+    }else
+        cout << "Friendship\n";
+
+    return 0;
+}
