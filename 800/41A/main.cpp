c788e2844e3817d56f352506b6d078cb8bc774ac
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-01-04 13:03:23 +0100

    41A

---
+++ 800/41A/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+
+	string s, t;
+
+	cin >> s >> t;
+
+
+
+ 	int n = t.length();
+
+	for (int i = 0; i < n / 2; i++)
+  	swap(t[i], t[n - i - 1]);
+
+	if(s == t)
+		cout << "YES\n";
+	else
+		cout << "NO\n";
+
+	return 0;
+}
